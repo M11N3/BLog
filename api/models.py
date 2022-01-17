580107ce14ef9e980cc0d1e076f143ac2a7c0149
M11N3 <m11n3Developer@gmail.com>
2022-01-17 11:38:15 +0300

    create model Article

--- api/models.py
+++ api/models.py
@@ -5,3 +5,15 @@ from django.db import models
 class Blog(models.Model):
     author = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
     subscribers = models.ManyToManyField(User, blank=True, related_name='subscriptions')
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    those_who_have_read = models.ManyToManyField(User, blank=True, related_name='read_articles')
+
+    def get_absolute_url(self):
+        return f"/article/{self.pk}"
+
---
+++ api/views.py
@@ -0,0 +1,2 @@
+from django.shortcuts import redirect, render
+from django.views.generic import View
