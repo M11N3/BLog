380c90af6fce9cb7aede2f872d43ea3bdb177b6b
M11N3 <m11n3Developer@gmail.com>
2022-01-17 11:57:54 +0300

    add singnal to sending mail to subscriber about new article

--- api/signals.py
+++ api/signals.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+from django.core.mail import send_mail
+from django.db.models.signals import post_save
 
-from .models import Blog
+from .models import Article, Blog
 
 
 def create_blog_by_user(sender, instance, created, **kwargs):
@@ -9,4 +11,17 @@ def create_blog_by_user(sender, instance, created, **kwargs):
         Blog.objects.create(author=instance)
 
 
+def send_mail_for_subscribers(sender, instance, created, **kwargs):
+    if created and settings.USE_EMAIL_SEND:
+        subscribers = instance.blog.subscribers.all().values_list("email", flat=True)
+        send_mail(
+            'New Article',
+            f'У автора @{instance.blog.author} вышла новая статья http://localhost:8000{instance.get_absolute_url()}',
+            settings.EMAIL_HOST_USER,
+            subscribers,
+            fail_silently=False
+        )
+
+
 post_save.connect(create_blog_by_user, sender=User, dispatch_uid="create_blog_by_user")
+post_save.connect(send_mail_for_subscribers, sender=Article, dispatch_uid="send_mail_for_subscribers")
