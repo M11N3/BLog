0e852a53500bd723698a829f707d72efbd6ada10
M11N3 <m11n3Developer@gmail.com>
2022-01-17 13:43:07 +0300

    add singnal for remove has read article after unsubscribe

--- api/signals.py
+++ api/signals.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 
 from .models import Article, Blog
 
@@ -23,5 +23,18 @@ def send_mail_for_subscribers(sender, instance, created, **kwargs):
         )
 
 
+def remove_has_read_articles_after_unsubscribe(sender, instance, pk_set, action, **kwargs):
+    if action == "pre_remove":
+        for user_id in pk_set:
+            article = instance.article_set.all()
+            user = User.objects.get(id=user_id)
+            user.read_articles.remove(*article)
+
+
 post_save.connect(create_blog_by_user, sender=User, dispatch_uid="create_blog_by_user")
 post_save.connect(send_mail_for_subscribers, sender=Article, dispatch_uid="send_mail_for_subscribers")
+m2m_changed.connect(
+    remove_has_read_articles_after_unsubscribe,
+    sender=Blog.subscribers.through,
+    dispatch_uid="remove_has_read_articles_after_unsubscribe"
+)
